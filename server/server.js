ed2281ed9de5e51c05221ef1fd3239b91914cf9e
Martin Guevara <martin_91@live.com.ar>
2023-06-07 01:38:56 -0300

    code improvement

--- server/server.js
+++ server/server.js
@@ -14,108 +14,94 @@ app.get('/', (req, res) => res.send(activeRooms))
 
 let activeRooms = []
 
+const getRoom = (client) => new Promise((resolve, reject) => {
+    client.rooms.forEach(clientRoom => {
+        if (clientRoom != client.id) {
+            const getRoomData = activeRooms.find(room => room.id == clientRoom)
+            resolve(getRoomData)
+        }
+    })
+})
+
 socketServer.on('connection', (client) => {
-    client.on("createRoom", async (nickname) => {
-        const roomID = 'room' + Math.floor(Math.random() * 1000)
-        await client.join(roomID)
-
-        socketServer.to(client.id).emit("roomCreated", roomID)
-
-        activeRooms.push({
-            id: roomID,
-            users: [{
-                clientID: client.id,
-                nickname: nickname,
-            }],
-            messages: [],
-            state: 'waiting'
-        })
-
-        setTimeout(() => socketServer.to(roomID).emit("clientConnected", [{ clientID: client.id, nickname: nickname }]), 100)
-    });
-
-    client.on("joinRoom", async (roomID, nickname) => {
-        const checkRoomExistence = activeRooms.find(room => room.id == roomID)
-
-        if (checkRoomExistence === undefined) {
-            socketServer.to(client.id).emit("log", `La sala ${roomID} no existe`)
-        } else {
+    client
+        .on("createRoom", async (nickname) => {
+            const roomID = 'room' + Math.floor(Math.random() * 1000)
             await client.join(roomID)
 
-            socketServer.to(client.id).emit("joinSuccess", roomID)
-            socketServer.to(roomID).emit("newMessage", '', `${nickname} entró a la sala`)
+            socketServer.to(client.id).emit("roomCreated", roomID)
+            activeRooms.push({ id: roomID, users: [{ clientID: client.id, nickname: nickname }], messages: [], state: 'waiting' })
+            setTimeout(() => socketServer.to(roomID).emit("clientConnected", [{ clientID: client.id, nickname: nickname }]), 100)
+        })
 
-            checkRoomExistence.users.push({ clientID: client.id, nickname: nickname })
-            setTimeout(() => socketServer.to(roomID).emit("clientConnected", checkRoomExistence.users), 100)
-        }
-    });
+        .on("joinRoom", async (roomID, nickname) => {
+            const checkRoomExistence = activeRooms.find(room => room.id == roomID)
 
-    client.on("leaveRoom", () => {
-        client.rooms.forEach(clientRoom => {
-            if (clientRoom != client.id) {
-                client.leave(clientRoom)
-                socketServer.to(client.id).emit("leaveSuccess")
+            if (checkRoomExistence === undefined) {
+                socketServer.to(client.id).emit("log", `La sala ${roomID} no existe`)
+            } else {
+                await client.join(roomID)
+
+                socketServer.to(client.id).emit("joinSuccess", roomID)
+                socketServer.to(roomID).emit("newMessage", '', `${nickname} entró a la sala`)
+
+                checkRoomExistence.users.push({ clientID: client.id, nickname: nickname })
+                setTimeout(() => socketServer.to(roomID).emit("clientConnected", checkRoomExistence.users), 100)
             }
         })
-    });
 
-    client.on("sendMessage", async (message) => {
-        client.rooms.forEach(clientRoom => {
-            if (clientRoom != client.id) {
-                const getRoom = activeRooms.find(room => room.id == clientRoom)
-                const userData = getRoom.users.find(user => user.clientID == client.id)
-
-                socketServer.to(clientRoom).emit("newMessage", client.id, userData.nickname, message)
-
-                if (getRoom !== undefined) {
-                    getRoom.messages.push({ clientID: client.id, nickname: userData.nickname, message: message })
-                }
-            }
+        .on("leaveRoom", async () => {
+            const clientRoom = await getRoom(client)
+            client.leave(clientRoom.id)
+            socketServer.to(client.id).emit("leaveSuccess")
         })
-    });
 
-    client.on("startGame", () => {
-        client.rooms.forEach(clientRoom => {
-            if (clientRoom != client.id) {
-                const getRoom = activeRooms.find(room => room.id == clientRoom)
-
-                if ((getRoom.users.length > 0) && (getRoom.state === 'waiting')) {
-                    const tokensTaken = []
-
-                    getRoom.users.forEach(user => {
-                        user.cards = []
-
-                        let pushedCounter = 0
-
-                        do {
-                            const number = Math.floor(Math.random() * 106) + 1
-                            const checkExistence = tokensTaken.some(token => token == number)
-
-                            if (!checkExistence) { 
-                                tokensTaken.push(number)
-                                user.cards.push(number)
-                                pushedCounter++
-                            }
-                        } while (pushedCounter != 14)
-
-                        console.log(user.cards);
-                    })
-                }
-
-                // let timeLeft = 11;
-
-                // function countdown() {
-                //     timeLeft--;
-                //     socketServer.to(clientRoom).emit("startGameCountdown", timeLeft)
-                //     if (timeLeft > 0) {
-                //         setTimeout(countdown, 1000);
-                //     }
-                // };
-
-                // setTimeout(countdown, 1000);
-            }
+        .on("sendMessage", async (message) => {
+            const clientRoom = await getRoom(client)
+            const userData = clientRoom.users.find(user => user.clientID == client.id)
+            socketServer.to(clientRoom.id).emit("newMessage", client.id, userData.nickname, message)
+            clientRoom.messages.push({ clientID: client.id, nickname: userData.nickname, message: message })
         })
-    });
+
+        .on("startGame", async () => {
+            const clientRoom = await getRoom(client)
+
+            if ((clientRoom.users.length > 0) && (clientRoom.state === 'waiting')) {
+                const tokensTaken = []
+
+                clientRoom.users.forEach(user => {
+                    user.cards = []
+
+                    let pushedCounter = 0
+
+                    do {
+                        const number = Math.floor(Math.random() * 106) + 1
+                        const checkExistence = tokensTaken.some(token => token == number)
+
+                        if (!checkExistence) {
+                            tokensTaken.push(number)
+                            user.cards.push(number)
+                            pushedCounter++
+                        }
+                    } while (pushedCounter != 14)
+
+                    console.log(user);
+                    console.log(user.cards);
+                })
+            }
+
+            // let timeLeft = 11;
+
+            // function countdown() {
+            //     timeLeft--;
+            //     socketServer.to(clientRoom).emit("startGameCountdown", timeLeft)
+            //     if (timeLeft > 0) {
+            //         setTimeout(countdown, 1000);
+            //     }
+            // };
+
+            // setTimeout(countdown, 1000);
+        })
 })
 
 socketServer.of("/").adapter.on("leave-room", (roomID, clientID) => {
@@ -136,5 +122,5 @@ socketServer.of("/").adapter.on("leave-room", (roomID, clientID) => {
 });
 
 const PORT = process.env.PORT || 8080
-const server = httpServer.listen(PORT, (req, res) => console.log(`[ Listening port: ${PORT} ]`))
+const server = httpServer.listen(PORT, () => console.log(`[ Listening port: ${PORT} ]`))
 server.on('error', error => console.log(`Error: ${error}`))
